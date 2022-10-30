f0c3e498eb835ccf3e4aec913b0ab17073d95147
Kevin Chang <kevinjchang98@gmail.com>
2022-10-29 18:34:04 -0700

    link to details page

---
+++ pages/details/lion/index.tsx
@@ -0,0 +1,28 @@
+import styles from '../../../styles/Home.module.css';
+
+export default function Lion() {
+    return (
+        <div className={styles.container}>
+            <main className={styles.description}>
+                <h1 className={styles.title}>Lions</h1>
+                <p>
+                    Lions are leaders. They are usually the bosses at
+                    work&#8230; or at least they think they are! They are
+                    decisive, bottom line folks who are observers, not watchers
+                    or listeners. They love to solve problems. They are usually
+                    individualists who love to seek new adventures and
+                    opportunities.
+                </p>
+
+                <p>
+                    Lions are very confident and self-reliant. In a group
+                    setting, if no one else instantly takes charge, the Lion
+                    will. Unfortunately, if they don’t learn how to tone down
+                    their aggressiveness, their natural dominating traits can
+                    cause problems with others. Most entrepreneurs are strong
+                    lions, or at least have a lot of lion in them.
+                </p>
+            </main>
+        </div>
+    );
+}
