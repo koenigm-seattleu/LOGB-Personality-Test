e6c830440001002f044a777f1669c90b0b4837a0
Kevin Chang <kevinjchang98@gmail.com>
2022-10-29 21:57:31 -0700

    render null if no data

--- pages/ScoreGraph.jsx
+++ pages/ScoreGraph.jsx
@@ -2,40 +2,44 @@ import RadarChart from 'react-svg-radar-chart';
 import 'react-svg-radar-chart/build/css/index.css';
 
 export default function ScoreGraph({ data }) {
-    const graphData = [
-        {
-            data: {
-                lion: data[0] / 40,
-                otter: data[1] / 40,
-                goldenretriever: data[2] / 40,
-                beaver: data[3] / 40,
+    if (data) {
+        const graphData = [
+            {
+                data: {
+                    lion: data[0] / 40,
+                    otter: data[1] / 40,
+                    goldenretriever: data[2] / 40,
+                    beaver: data[3] / 40,
+                },
+                meta: { color: 'blue' },
             },
-            meta: { color: 'blue' },
-        },
-    ];
+        ];
 
-    const captions = {
-        // columns
-        lion: 'L ' + data[0],
-        otter: 'O ' + data[1],
-        goldenretriever: 'G ' + data[2],
-        beaver: 'B ' + data[3],
-    };
-    return (
-        <RadarChart
-            captions={captions}
-            data={graphData}
-            size={300}
-            options={{
-                scales: 4,
-                captionMargin: 20,
+        const captions = {
+            // columns
+            lion: 'L ' + data[0],
+            otter: 'O ' + data[1],
+            goldenretriever: 'G ' + data[2],
+            beaver: 'B ' + data[3],
+        };
+        return (
+            <RadarChart
+                captions={captions}
+                data={graphData}
+                size={300}
+                options={{
+                    scales: 4,
+                    captionMargin: 20,
 
-                captionProps: () => ({
-                    className: 'caption',
-                    textAnchor: 'middle',
-                    fontSize: 16,
-                }),
-            }}
-        />
-    );
+                    captionProps: () => ({
+                        className: 'caption',
+                        textAnchor: 'middle',
+                        fontSize: 16,
+                    }),
+                }}
+            />
+        );
+    } else {
+        return null;
+    }
 }
