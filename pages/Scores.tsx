214b9457fa62af247aac3674fc086e1d83000101
Kevin Chang <kevinjchang98@gmail.com>
2022-10-29 18:18:25 -0700

    implement getting main animal

--- pages/Scores.tsx
+++ pages/Scores.tsx
@@ -4,8 +4,19 @@ import styles from '../styles/Home.module.css';
 interface ScoresProps {
     scores: Array<Array<number>>;
 }
+
+enum Categories {
+    'Lion' = 0,
+    'Otter' = 1,
+    'Golden Retriever' = 2,
+    'Beaver' = 3,
+}
+
 export default function Scores({ scores }: ScoresProps) {
     const [scoreSum, setScoreSum] = useState<Array<number>>([0, 0, 0, 0]);
+    const [primaryCategory, setPrimaryCategory] = useState<Categories>(
+        Categories['Lion']
+    );
 
     useEffect(() => {
         let newScoreSum = [0, 0, 0, 0];
@@ -16,7 +27,18 @@ export default function Scores({ scores }: ScoresProps) {
             }
         }
 
+        let maxCategory = -1;
+        let maxCategoryCount = -1;
+
+        for (let i = 0; i < 4; i++) {
+            if (maxCategoryCount < newScoreSum[i]) {
+                maxCategory = i;
+                maxCategoryCount = newScoreSum[i];
+            }
+        }
+
         setScoreSum(newScoreSum);
+        setPrimaryCategory(maxCategory);
     }, [scores]);
 
     return (
@@ -26,6 +48,8 @@ export default function Scores({ scores }: ScoresProps) {
                 L: {scoreSum[0]} O: {scoreSum[1]} G: {scoreSum[2]} B:{' '}
                 {scoreSum[3]}{' '}
             </p>
+            <h2>Main animal:</h2>
+            <p>{Categories[primaryCategory]}</p>
         </div>
     );
 }
